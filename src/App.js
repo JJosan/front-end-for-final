708257cb2888478cbd37f7d70915325b06d5b933
JJosan <nosajn123@gmail.com>
2022-05-24 16:13:29 -0700

    yes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Navbar, NavbarBrand, NavItem } from 'reactstrap'
+
+const tempID = "628c6c0f3b8adbfca80804b9"
+
+const apiVersion = "v1"
+
+const test = () => {
+  console.log("host new group")
+  fetch(`http://localhost:3000/api/${apiVersion}/trips/add`,{
+    method: "POST"
+})
+.then(response => {
+  response.json().then((data) => {
+      console.log(JSON.stringify(data.ID))
+      document.getElementById("joinID").innerText=`Join ID: ${data.ID}`;
+  }).catch((err) => {
+      console.log(err)
+  })
+})
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>hello</h1>
+        
+        <Navbar>
+          <NavbarBrand>
+            DivvyUp
+          </NavbarBrand>
+          <NavItem>
+            Home
+          </NavItem>
+          <NavItem>
+            How to use
+          </NavItem>
+          <NavItem>
+            About us
+          </NavItem>
+        </Navbar>
     </div>
   );
 }
