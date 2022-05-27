2cb4afa52b05b7267b3c9c08cab992624789b64e
mark0822 <mark20020822@gmail.com>
2022-05-26 23:23:22 -0700

    working on shoppingPage

--- src/Components/ShoppingPage/ShoppingPage.js
+++ src/Components/ShoppingPage/ShoppingPage.js
@@ -1,71 +1,88 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './ShoppingPage.css'
-import {List, Input, Button } from 'reactstrap'
+import {List, Input, Button} from 'reactstrap'
 
 let tripID = null
 const apiVersion="v1"
 
+
 function ShoppingPage() {
-  getTripID();
+  
+  getTripID(showList);
+
   return (
     <div className='ShoppingPage'>
-        {/* <List type="inline" id="shoppingList" onLoad={showList}></List> */}
+        
         <Input name="itemNameInput" id="itemNameInput" placeholder="Input item name" />
         <Input name="itemQuantityInput" id="itemQuantityInput" placeholder="Input item quantity" />
         <Button onClick={addItem}>add item</Button>
-        <Button onClick={getTripID}>get tripID</Button>
+        <Button onClick={showList}>Refresh List</Button>
         <div id="tripID"></div>
+        <List type="inline" id="shoppingList"></List>
     </div>
   )
-}
 
 async function addItem() {
-  let item = document.getElementById("itemName").value;
-  let quantity = document.getElementById("itemQuantity").value;
+  let item = document.getElementById("itemNameInput").value;
+  let quantity = document.getElementById("itemQuantityInput").value;
   console.log(tripID)
   try {
        const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' }
         };
-        fetch(`api/${apiVersion}/items/add?item=${item}&quantity=${quantity}&tripID=${tripID}`, requestOptions)
+        await fetch(`api/${apiVersion}/items/add?item=${item}&quantity=${quantity}&tripID=${tripID}`, requestOptions)
             .then(response => response.json())
             .then(data => console.log(data));
-      // await fetchJSON(`api/${apiVersion}/items/add?item=${item}&quantity=${quantity}&tripID=${tripID}`, {
-      //     method: "POST"
-      // })
+        showList();
   } catch(error) {
       throw(error)
   }
 }
 
-// async function showList(){
-//   try {
-//     let receipt = await fetch(`api/${apiVersion}/items/receipt?tripID=${tripID}`)
-//     var shoppingList = document.getElementById("shoppingList")
-//     for (let i = 0; i < receipt.length; i++) {
-//         var listItem = document.createElement(ListInlineItem)
-//         listItem.innerHTML = `${receipt[i].NameOfItem}:${receipt[i].Quantity}`
-//         shoppingList.appendChild(listItem);
-//     }
-//   }catch(error) {
-//     throw(error)
-//   }
-// }
 
-async function getTripID(){
+async function showList(){
+  try {
+    const testHandler = (id) => {
+      console.log(`button clicked: ${id}`)
+      
+    }
+    console.log(tripID)
+    await fetch(`api/${apiVersion}/items/receipt?tripID=${tripID}`)
+    .then(response => response.json())
+    .then(receipt => {
+      var shoppingList = document.getElementById("shoppingList")
+      shoppingList.innerHTML = ''
+      for (let i = 0; i < receipt.length; i++) {
+        var listItem = document.createElement("ListInlineItem")
+        listItem.innerHTML = `${receipt[i].NameOfItem}:${receipt[i].Quantity} <br>`
+        var buttonItem = document.createElement("Button")
+        // buttonItem.className = "btn"
+        buttonItem.innerHTML = "delete"
+        buttonItem.id = receipt[i]._id;
+        buttonItem.addEventListener("click", testHandler(buttonItem.id));
+        shoppingList.appendChild(buttonItem);
+        shoppingList.appendChild(listItem);
+      } 
+    })   
+  }catch(error) {
+    throw(error)
+  }
+}
+
+async function getTripID(callback){
   try{
-    fetch(`api/${apiVersion}/trips/tripID`)
+    await fetch(`api/${apiVersion}/trips/tripID`)
     .then(response => response.json())
     .then(data => {
       tripID = data.tripID
       document.getElementById("tripID").innerHTML = data.tripID
     })
-    //
+    callback();
   }catch(error){
     throw(error)
   }
 }
 
-
-export default ShoppingPage
+}
+export default ShoppingPage;
