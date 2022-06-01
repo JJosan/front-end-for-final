e086997b16ae04af6852c903052aa22d333c54cd
iwangy <35878784+iwangy@users.noreply.github.com>
2022-05-31 17:26:30 -0700

    styled main page

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './MainPage.css'
-import {Button, Input} from 'reactstrap'
+import {Button, Input, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap'
 import { Link } from 'react-router-dom'
 
 const apiVersion = "v1"
 
+
+
 function MainPage() {
+
+    const [modal, setModal] = useState(false);
+    const toggleModal = () => setModal(!modal);
 
     async function createNewTrip(){
         await fetch(`api/${apiVersion}/trips/add`,{
@@ -41,14 +46,37 @@ function MainPage() {
     return (
         <div className='MainPage_container'>
             <div className='MainPage'>
-                <Link to='shoppingpage'><Button onClick={createNewTrip}>Host New Group</Button></Link>
-                <Input name="tripIDInput" id="tripIDInput" placeholder="Input TripID" />
-                <Link to='/shoppingpage'><Button onClick={joinTrip}>Join Group</Button></Link>
-                <div id="joinID"></div>
+                <div className="TitlePage">DivvyUp</div>
+                <Link to='shoppingpage'><Button id='MainPage_CreateTrip' onClick={createNewTrip}>Host New Group</Button></Link>
+                <div>
+                <Button id='MainPage_JoinTrip' onClick={toggleModal}>
+                    Join Group
+                </Button>
+                <Modal isOpen={modal}>
+                    {/* <ModalHeader> Modal title </ModalHeader> */}
+                    <ModalBody>  
+                        <Input name="tripIDInput" id="tripIDInput" placeholder="Input TripID" />
+                    </ModalBody>
+                    <ModalFooter>
+                        <Link to='shoppingpage'>
+                            <Button color="primary" onClick={joinTrip}>
+                                Join Group
+                            </Button>
+                        </Link>
+                        {' '}
+                        <Button onClick={toggleModal}>
+                            Cancel
+                        </Button>
+                    </ModalFooter>
+                </Modal>
             </div>
+            </div>
+                
+            
             {/* server blows up if the same person makes more than 1 trip */}
             
         </div>
+        
   )
 }
 
