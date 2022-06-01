8ff1308419a6b6d233ca3c2b5e378d8054bd23dc
mark0822 <mark20020822@gmail.com>
2022-05-31 23:41:18 -0700

    broken code

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import './MainPage.css'
 import {Button, Input} from 'reactstrap'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
-const apiVersion = "v1"
+const apiVersion = "v1"    
+
 
 function MainPage() {
+    let navigate = useNavigate();
 
+    // checkUser();
+
+    // async function checkUser() {
+    //     try {
+    //         console.log("function called")
+    //         await fetch(`api/${apiVersion}/trips/userStatus`)
+    //     } catch(error) {
+    //         throw(error)
+    //     }
+    // }
+
+    // function delay(time) {
+    //     return new Promise(resolve => setTimeout(resolve, time));
+    // }
+    // delay(10000);
+    // checkUserStatus()
+
+    // async function checkUserStatus(){
+    //     await fetch(`api/${apiVersion}/trips/userStatus`)
+    //     .then(response => {
+    //         response.json().then((data) => {
+    //             if(data.tripID!=null){
+    //                 navigate("/shoppingpage")
+    //             }
+    //         }).catch((err) => {
+    //             console.log(err)
+    //         })
+    //     })
+    // }
     async function createNewTrip(){
         await fetch(`api/${apiVersion}/trips/add`,{
             method: "POST"
@@ -19,8 +50,10 @@ function MainPage() {
                 console.log(err)
             })
         })
+        navigate("/shoppingpage");
     }
-    
+
+
     async function joinTrip(){
         let tripID = document.getElementById("tripIDInput").value;
         console.log(tripID)
@@ -33,21 +66,22 @@ function MainPage() {
             await fetch(`api/${apiVersion}/trips/addUser`, requestOptions)
                 .then(response => response.json())
                 .then(data => console.log(data));
+            navigate("/shoppingpage");
         } catch(error) {
             throw(error)
         }
     }
 
     return (
-        <div className='MainPage_container'>
+        <div className='MainPage_container' onLoad="/api/v1/trips/userstatus">
             <div className='MainPage'>
-                <Link to='shoppingpage'><Button onClick={createNewTrip}>Host New Group</Button></Link>
+                <Button onClick={createNewTrip}>Host New Group</Button>
                 <Input name="tripIDInput" id="tripIDInput" placeholder="Input TripID" />
-                <Link to='/shoppingpage'><Button onClick={joinTrip}>Join Group</Button></Link>
+                <Button onClick={joinTrip}>Join Group</Button>
+                <Button href="/api/v1/trips/userstatus">test</Button>
                 <div id="joinID"></div>
             </div>
-            {/* server blows up if the same person makes more than 1 trip */}
-            
+            {/* server blows up if the same person makes more than 1 trip */} 
         </div>
   )
 }
